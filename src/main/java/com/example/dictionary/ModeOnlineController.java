d99030bbb726c7a4a47242c4d27e18f30ea859e0
DangNguyen <144325359+DangNguyen38@users.noreply.github.com>
2023-11-19 11:15:04 +0700

    Update ModeOnlineController.java

--- src/main/java/com/example/dictionary/ModeOnlineController.java
+++ src/main/java/com/example/dictionary/ModeOnlineController.java
@@ -3,21 +3,63 @@ package com.example.dictionary;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.web.WebView;
+import javafx.scene.control.ToggleGroup;
+
+import java.io.IOException;
 
 public class ModeOnlineController {
-    public JFXHamburger hamburger;
-    public JFXDrawer drawer;
-    public TextField enWord;
-    public TextArea viWord;
+    @FXML
+    protected JFXHamburger hamburger;
+    @FXML
+    protected JFXDrawer drawer;
+    @FXML
+    protected TextField enWord;
+    @FXML
+    protected WebView viWord;
+    @FXML
+    protected RadioButton EnToVi;
+    @FXML
+    protected RadioButton ViToEn;
+    public final boolean check = true;
+    @FXML
+    ToggleGroup toggleGroup = new ToggleGroup();
+
 
     public void onClickSpeakerButtonEn(ActionEvent actionEvent) {
+        String source = enWord.getText();
+        if (check) {
+            TextToSpeech.playSoundGoogleTranslateViToEn(source);
+
+        }
     }
 
-    public void onClickSpeakerButtonVi(ActionEvent actionEvent) {
+     public void onClickSpeakerButtonVi() {
+         String source = viWord.getEngine().getDocument().getTextContent();;
+        if (check) {
+            TextToSpeech.playSoundGoogleTranslateEnToVi(source);
+        }
     }
 
-    public void onClickedButtonTranslate(ActionEvent actionEvent) {
-    }
+
+
+     public void onClickedButtonTranslate(ActionEvent event) {
+         String text = enWord.getText();
+         if (EnToVi.isSelected()) {
+             try {
+                 viWord.getEngine().loadContent(TranslateAPI.googleTranslate("en", "vi", text));
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         } else {
+             try {
+                 viWord.getEngine().loadContent(TranslateAPI.googleTranslate("vi", "en", text));
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }
+     }
+
 }
